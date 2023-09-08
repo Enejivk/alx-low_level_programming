88bccffe9cbe1bc27b387c25bdef0453f10fd869
Enejivk <enejivic@gmail.com>
2023-09-07 18:43:47 -0700

    this is my actual first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** main - the main put to the screen
+ * the argument that was assigned
+ * Return: At th end is zero
+ */
+int main (void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
