689c8c5e943dd9dc208453f3ae956424f804fe6e
Enejivk <enejivic@gmail.com>
2023-09-07 19:23:06 -0700

    this is my actual first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry point of the program
  *
- * Return: 0 on success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+	/* Print a message to the console */
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
 
