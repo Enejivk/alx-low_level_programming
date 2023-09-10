54396982d2901e0670a0ea11ae833853c7cf9d19
Enejivk <enejivic@gmail.com>
2023-09-10 14:20:49 -0700

    this is to print without printf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point of the code
+ *
+ * Return: return is always 0
+ */
+int main(void)
+{
+	int v;
+
+	for (v = 0; v <= 10; v++)
+	{
+		putchar ('0' + v);
+	}
+		putchar('\n');
+return (0);
+}
