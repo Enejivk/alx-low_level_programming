193e273386628e3dc26d9d6396cb3308d4a630e9
Enejivk <enejivic@gmail.com>
2023-09-10 03:55:49 -0700

    this is a number commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point of the code
+ *
+ * Return: return is always 0
+ */
+int main(void)
+{
+	int v;
+
+	for (v = 0; v <= 10; v++)
+	{
+		printf("%d", v);
+	}
+		printf("\n");
+return (0);
+}
