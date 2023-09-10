eb432387ae7eac27fc18a908545c032432ae11eb
Enejivk <enejivic@gmail.com>
2023-09-10 15:25:45 -0700

    this is number and alphbet commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point of the code
+ *
+ * Return: return is always 0
+ */
+int main(void)
+{
+	int v;
+
+	for (v = 48; v < 58; v++)
+	{
+		putchar (v);
+	}
+	for (v =97; v < 103; v++)
+	{
+		putchar (v);
+	}
+		putchar('\n');
+return (0);
+}
