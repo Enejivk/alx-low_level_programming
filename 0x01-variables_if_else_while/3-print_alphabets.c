258f14e32b6a0acf749212dac524e246941f72e9
Enejivk <enejivic@gmail.com>
2023-09-10 02:09:40 -0700

    this is alphbeth commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point of the code
+ *
+ * Return: return is always 0
+ */
+int main(void)
+{
+	char v;
+	for (v = 'a'; v <= 'z'; v++)
+	{
+		putchar (v);
+		putchar ('\n');
+	}
+return (0);
+}
