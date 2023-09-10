88ee3514844005e838b2eb00be01f42bdf9b1f67
Enejivk <enejivic@gmail.com>
2023-09-10 05:00:12 -0700

    this is a revers alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point of the code
+ *
+ * Return: return is always 0
+ */
+int main(void)
+{
+	char v;
+
+	for (v = 'z'; v >= 'a'; v--)
+	{
+		putchar (v);
+	}
+		putchar('\n');
+return (0);
+}
