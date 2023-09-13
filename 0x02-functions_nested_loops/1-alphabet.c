a10faebacecfa3452ebc780fc99509e1e9fc1af6
Enejivk <enejivic@gmail.com>
2023-09-13 01:13:59 -0700

    this is to write the alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
+
+void print_alphabet(void)
+{
+	char v;
+
+	for (v = 'a'; v <= 'z'; v++)
+		_putchar(v);
+
+	_putchar('\n');
+}
+
