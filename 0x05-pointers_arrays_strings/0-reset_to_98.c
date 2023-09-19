477ab9562c8e45013da7d02924e169fcb018b486
Enejivk <enejivic@gmail.com>
2023-09-19 15:47:31 -0700

    To reset the value of n commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98 - this is the name of the function
+ *@n: - n holds the value of the variable
+ *Description: this code reset the value originaly assigned to n;
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
