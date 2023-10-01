036f9bfb276a78f89236ac62affb2e7a56985837
Enejivk <enejivic@gmail.com>
2023-10-01 03:12:45 -0700

    Recursion to print string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+/**
+*int _strlen_recursion- recursion to return string length
+*@s: This is the pointer to the string to be determined
+*/
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return 0;
+}
+else
+{
+return 1 + _strlen_recursion(s + 1);
+}
+}
