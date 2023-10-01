25ebf28c73d645ead9ab82bd77ff35714a9997be
Enejivk <enejivic@gmail.com>
2023-10-01 04:24:56 -0700

    This is my strlen Commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,18 @@
+#include "main.h"
 /**
-*int _strlen_recursion- recursion to return string length
-*@s: This is the pointer to the string to be determined
+* _strlen_recursion - recursion to return string length
+* @s: This is the pointer to the string to be determined
+*Return: It returns an integer variable
 */
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
-return 0;
-}
+return (0);
+{
 else
 {
-return 1 + _strlen_recursion(s + 1);
+(return 1) + _strlen_recursion(s + 1);
 }
 }
+
