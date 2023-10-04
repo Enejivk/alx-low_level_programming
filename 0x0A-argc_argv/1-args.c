14ad1853b0552547aac2e7432b5c4379bbe459f9
Enejivk <enejivic@gmail.com>
2023-10-04 00:28:23 -0700

    Argument count commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main- This is the function
+*@argc: argc means argument count
+*@argv: argv means argument vector
+*Return: it alway 0
+*/
+
+int main(int argc, char *argv[])
+{
+(void) argv; /*Ignore argv*/
+printf("%d\n", argc);
+return (0);
+}
