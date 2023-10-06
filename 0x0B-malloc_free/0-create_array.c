dbfd509cb353a908c9b92bd0a57e9948cab3a447
Enejivk <enejivic@gmail.com>
2023-10-06 09:29:22 -0700

    This is my first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*create_array- This function create streams of arrays
+*@size: This size holds the size of memory to be allocated
+*@c:This char is latter initialized with something else
+*/
+char *create_array(unsigned int size, char c)
+{
+char *arrays;
+unsigned int i;
+
+arrays = malloc(sizeof(char) * size);
+if (size == 0 || arrays == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+arrays[i] = c;
+return (arrays);
+}
