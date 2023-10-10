9ab8eff82d39f240a27bd1324e35b0756c9bb2a4
Enejivk <enejivic@gmail.com>
2023-10-10 15:15:36 -0700

    Init commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+*init_dog - This is the title of the fuction
+*@d :This is the pointer to the dog varibel
+*@name: the element that holds the name
+*@age: the element that holds the age
+*@owner: this hols the name of the owner
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
