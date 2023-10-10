cc945e93b599f5ccb1f5e148a94a3812a4215c5a
Enejivk <enejivic@gmail.com>
2023-10-10 09:44:46 -0700

    This is my header file commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+*struct dog - This is structure descrips the feature of the dog
+*@name: The name of the dog
+*@age: the age of the dog
+*@owner: the owner of the dog
+*
+*Description: This hods all the feature of the dog
+*/
 struct dog
 {
 char *name;
