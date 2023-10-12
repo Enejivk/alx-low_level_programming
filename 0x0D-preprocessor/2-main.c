5201affba7f7813a1bc727fe30d44d97c58100d8
Enejivk <enejivic@gmail.com>
2023-10-12 08:40:53 -0700

    THE LAST COMMIT

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the fil
+ *
+ * Return: It always return 0 when the code is succefull
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
