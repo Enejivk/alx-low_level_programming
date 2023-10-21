99ea2a6acb1d351afbf898614af66a28a1d474c7
Enejivk <enejivic@gmail.com>
2023-10-21 00:40:13 -0700

    This is returns the linlist lent

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-*List_len- This functioon get the length of a linklist
+*list_len- This functioon get the length of a linklist
 *@h: pointer to the head node
 *Return: it returns count
 */
 size_t list_len(const list_t *h)
 {
 size_t count = 0;
-while(h != NULL)
+while (h != NULL)
 {
 h = h->next;
 count++;
