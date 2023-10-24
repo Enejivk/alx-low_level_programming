ae346bd1ba2388911cd91eead6b65a77ce4209f7
Enejivk <enejivic@gmail.com>
2023-10-24 13:50:23 -0700

    commit for the third task

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,31 +7,31 @@
  * add_nodeint_end- Add node at the end of linklist
  * @head: This is a pointer to the pointer(double) of the first node
  * @n: This holds the value of n;
+ * Return: It returns newnode
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node;
 	listint_t *curr;
-	
+
 	new_node = (listint_t *)malloc(sizeof(listint_t));
-	if(new_node == NULL)
+	if (new_node == NULL)
 	{
 		exit(1);
 	}
 	new_node->n = n;
 	new_node->next = NULL;
-	if(*head == NULL)
+	if (*head == NULL)
 	{
 		*head = new_node;
-		return new_node;
+		return (new_node);
 	}
 	curr = *head;
-	while(curr->next != NULL)
+	while (curr->next != NULL)
 	{
 		curr = curr->next;
 	}
 	curr->next = new_node;
-	return new_node;
-
+	return (new_node);
 }
