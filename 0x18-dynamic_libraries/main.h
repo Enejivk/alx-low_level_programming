1f5056b6530b4edc5b4c5e6f1a99d8767bb3a14c
enejivk <enejivic@gmail.com>
2023-12-12 03:30:09 +0100

    Dynamic library

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
+void _puts(char *str);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
@@ -20,4 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
