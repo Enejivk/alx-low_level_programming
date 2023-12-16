f667ee4371fafd0715d8524c74952e400c9f6cff
enejivk <enejivic@gmail.com>
2023-12-16 11:51:46 +0100

    Add element in the begining

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *add_dnodeint- Add element in the begining
+ * @head: double pointer to head
+ * @n: contain the value be passed to the new new node
+ *Return: It returns the address of the new_node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (0);
+	}
+	(*head)->prev = new_node;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
