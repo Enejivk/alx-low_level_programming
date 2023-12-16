26a0e3117b811ada2b8ce55d26ff6f21884292b2
enejivk <enejivic@gmail.com>
2023-12-16 18:52:49 +0100

    Add element at the begining of a linklist

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,27 +1,26 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
- * @head: A pointer to the head of the dlistint_t list.
- * @n: The integer for the new node to contain.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
+ * add_dnodeint - Add element at the beginning of a dlistint_t list.
+ * @head: Double pointer to the head.
+ * @n: Value to be stored in the new node.
+ * Return: Address of the new node, or NULL if it fails.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-	if (*head != NULL)
-		(*head)->prev = new;
-	*head = new;
-
-	return (new);
+dlistint_t *new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+new_node->next = NULL;
+new_node->prev = NULL;
+if (*head != NULL)
+{
+(*head)->prev = new_node;
+new_node->next = *head;
+}
+*head = new_node;
+return (new_node);
 }
