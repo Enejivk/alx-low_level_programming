fdb34e29fb5f018e46da47a2f96db2e4b8df6b7d
enejivk <enejivic@gmail.com>
2023-12-16 12:04:14 +0100

    Add element at the begining

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,6 +8,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
@@ -19,6 +20,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	(*head)->prev = new_node;
 	new_node->next = *head;
 	*head = new_node;
-
-	return (new_node);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+return (new_node);
+	}
 }
