4f8511a7a5287d289894bbf60f4eb88ca5bb2d76
enejivk <enejivic@gmail.com>
2023-12-17 21:19:19 +0100

    Add element at a specific index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index- add node at a specific index
+ * @h: pointer to pointer to head
+ * @idx: index where the value will be added
+ * @n: the value to be added to the node that was inserted
+ * Return: it returns address to the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i;
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	dlistint_t *tmp = *h;
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < idx; i++)
+	{
+		tmp = tmp->next;
+	}
+	new_node->n = n;
+	new_node->next = tmp;
+	new_node->prev = tmp->prev;
+	tmp->prev->next = new_node;
+	tmp->prev = new_node;
+	return (new_node);
+}
