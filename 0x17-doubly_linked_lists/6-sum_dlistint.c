0aea164edb410625d35e70039fda861c603319a9
enejivk <enejivic@gmail.com>
2023-12-17 07:01:28 +0100

    all all element of a linklist

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -2,19 +2,19 @@
 /**
 	* sum_dlistint- Thpis function sum all the element of a linklist
 	* @head: head contains the address of the first element
-	* Return: it returns the sum of all the element 
+	* Return: it returns the sum of all the element
 */
 int sum_dlistint(dlistint_t *head)
 {
 int sum = 0;
 if (head == NULL)
 {
-return 0;
+return (0);
 }
-while(head != NULL)
+while (head != NULL)
 {
 sum += head->n;
 head = head->next;
 }
-return sum;
+return (sum);
 }
