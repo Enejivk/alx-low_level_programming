8c195305f79d34b05ece08a8c44701b2931ccbc9
enejivk <enejivic@gmail.com>
2023-12-17 21:56:06 +0100

    Add element at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,22 +8,32 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-	dlistint_t *tmp = *h;
+unsigned int i;
+dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+dlistint_t *tmp = *h;
 
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < idx; i++)
-	{
-		tmp = tmp->next;
-	}
-	new_node->n = n;
-	new_node->next = tmp;
-	new_node->prev = tmp->prev;
-	tmp->prev->next = new_node;
-	tmp->prev = new_node;
-	return (new_node);
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+if (idx == 0)
+{
+(*h)->prev = new_node;
+new_node->next = *h;
+*h = new_node;
+return (new_node);
+}
+else
+{
+for (i = 0; i < idx; i++)
+{
+tmp = tmp->next;
+}
+new_node->next = tmp;
+new_node->prev = tmp->prev;
+tmp->prev->next = new_node;
+tmp->prev = new_node;
+return (new_node);
+}
 }
