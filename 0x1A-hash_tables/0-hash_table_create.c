da3f9b9fc87059beb570e3e37030dcbd3da0ae6d
enejivk <enejivic@gmail.com>
2023-12-22 17:31:29 +0100

    add element

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,20 +3,24 @@
  * hash_table_create- This function create a hash table
  * @size: This contain the size of the table
  * Return: It is going to return hash_table with the recomended size
-*/
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *table;
-table = (hash_table_t *)malloc(sizeof(hash_table_t));
-if (table == NULL)
-{
-return (NULL);
-}
-table->size = size;
-table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t));
-if (table->array == NULL)
-{
-return (NULL);
-}
-return (table);
+	hash_table_t *table;
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->size = size;
+	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		return (NULL);
+	}
+	for(i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
 }
