1fec5d001094c583bc4bc300288a622d8ef77dc5
enejivk <enejivic@gmail.com>
2023-12-23 15:07:01 +0100

    djb2 Algorithm

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2-This function return and index of a hashtablbes
+ * @str: This is a pointer to the string:
+ * Return: This return a hash table
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash = 5381;
+int i;
+int c = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+c = str[i];
+hash = hash * 33 + c;
+}
+return (hash);
+}
