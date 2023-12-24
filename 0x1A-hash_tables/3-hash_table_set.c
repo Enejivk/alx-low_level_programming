87a03007ceadb33d282b86e70a5afcf6de24c2e0
enejivk <enejivic@gmail.com>
2023-12-24 15:33:39 +0100

    Insert value to the hashtable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,34 +5,33 @@
  * @value: This is the value of item to be added
  * @ht: This is the hashtable
  * Return: It return 1 if succefull and return 0 otherwise
-*/
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    long int index;
-    hash_node_t *new_node, *current;
+	long int index;
+	hash_node_t *new_node, *current;
 
-    if (key == NULL || *key == '\0'|| ht == NULL)
-        return 0;
-    index = key_index((unsigned char *)key, ht->size);
-    new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
-    if(new_node == NULL)
-        return (0);
-    new_node->key = strdup(key);
-    if(new_node->key == NULL)
-    {
-        free(new_node);
-        return(0);
-    }
-    new_node->value = strdup(value);
-    if(new_node->value == NULL)
-    {
-        free(new_node);
-        return(0);
-    }
-
-    new_node->next = NULL;
-    current = ht->array[index];
-    ht->array[index] = new_node;
-    new_node->next = current;
-    return (1);
+	if (key == NULL || *key == '\0' || ht == NULL)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->next = NULL;
+	current = ht->array[index];
+	ht->array[index] = new_node;
+	new_node->next = current;
+	return (1);
 }
