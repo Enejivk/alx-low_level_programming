2ff3e2aa3fad41d5ead176605440067ed7d7f1f8
enejivk <enejivic@gmail.com>
2023-12-24 16:22:01 +0100

    search for element

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get-This search for element in a hashtable
+ * @ht: This is the hash table
+ * @key: This is the key
+ * Return: It return a character if succefull
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+			current = current->next;
+		}
+	}
+	return (NULL);
+}
