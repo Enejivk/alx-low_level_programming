c66815ef86a4a8457875894fcb2394e692e4327d
enejivk <enejivic@gmail.com>
2024-04-10 23:49:55 +0100

    linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+ *linear_search - perform a linear search on unsorted array
+ *@array: list of arrays to be searched
+ *@size: The size of the array
+ *@value: The target Value to return
+ *Return: It return a match value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (array[i]);
+		}
+	}
+	return (-1);
+}
